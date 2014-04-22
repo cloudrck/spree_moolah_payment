3e3c5f357fe7f644881a683c6c0504adcd563b6a
Daejuan Jacobs <djacobs@cloudrck.com>
2014-04-21 22:55:56 -0500

    Added QRK

--- app/controllers/spree/moolah_controller.rb
+++ app/controllers/spree/moolah_controller.rb
@@ -2,63 +2,64 @@ require 'moolah'
 
 module Spree
 	class MoolahController < StoreController
-		protect_from_forgery :except => :callback
+	protect_from_forgery :except => :callback
 
-		def page
-			order = current_order || raise(ActiveRecord::RecordNotFound)
-			cypto_curr =params[:curr]
+	def page
+		order = current_order || raise(ActiveRecord::RecordNotFound)
+		cypto_curr =params[:curr]
+		begin
+			#button_name = Spree.t("button.name", :scope => :moolah, :site_name => Spree::Config.site_name, :order_number => order.number)
+			case cypto_curr
+			when "BTC"
+				coin = payment_method.preferred_btc_guid
+			when "LTC"
+				coin = payment_method.preferred_ltc_guid
+			when "DOGE"
+				coin = payment_method.preferred_doge_guid
+			when "VTC"
+				coin = payment_method.preferred_vtc_guid
+			when "AUR"
+				coin = payment_method.preferred_aur_guid
+			when "DRK"
+				coin = payment_method.preferred_drk_guid
+			when "MAX"
+				coin = payment_method.preferred_max_guid
+			when "MINT"
+				coin = payment_method.preferred_mint_guid
+			when "QRK"
+				coin = payment_method.preferred_qrk_guid
+			end
+			#Generate API Request for Moolah Payment URL
+			moolah_options = { :query => {
+				:guid => coin,
+				:currency => order.currency,
+				:amount => order.total,
+				:product => "Order No #{order.number}", #order id
+				:return=> moolah_success_url,
+				:ipn=> moolah_callback_url       
+			}}
 
-			begin
-				#button_name = Spree.t("button.name", :scope => :moolah, :site_name => Spree::Config.site_name, :order_number => order.number)
-				case cypto_curr
-				when "BTC"
-					coin = payment_method.preferred_btc_guid
-				when "LTC"
-					coin = payment_method.preferred_ltc_guid
-				when "DOGE"
-					coin = payment_method.preferred_doge_guid
-				when "VTC"
-					coin = payment_method.preferred_vtc_guid
-				when "AUR"
-					coin = payment_method.preferred_aur_guid
-				when "DRK"
-					coin = payment_method.preferred_drk_guid
-				when "MAX"
-					coin = payment_method.preferred_max_guid
-				when "MINT"
-					coin = payment_method.preferred_mint_guid
-				end
-				#Generate API Request for Moolah Payment URL
-				moolah_options = { :query => {
-					:guid => coin,
-					:currency => order.currency,
-					:amount => order.total,
-					:product => "Order No #{order.number}", #order id
-					:return=> moolah_success_url,
-					:ipn=> moolah_callback_url       
-				}}
-
-				response = moolah.generate_url_payment(moolah_options) #Execute
+			response = moolah.generate_url_payment(moolah_options) #Execute
         
-				### Add a "processing" payment that is used to verify the callback
-				payment = order.payments.create({:amount => order.total,
-						:source => Spree::MoolahCheckout.create({
-								:order_id => order.number,
-								:status => "processing",
-								:transaction_id => response['tx']
-						}),
-        				:payment_method => payment_method })
-        			payment.started_processing!
-        		###########
-        		redirect_to "https://moolah.io/api/tx/#{response['tx']}"
-        		#redirect_to "#{response}"
-        		#For Production use
-        		#rescue => e
-        		#Rails.logger.error e
-        		# Redirect back to checkout so buyer can choose alternative payment method
-        		#redirect_to checkout_state_path(order.state)
-        		end
+			### Add a "processing" payment that is used to verify the callback
+			payment = order.payments.create({:amount => order.total,
+					:source => Spree::MoolahCheckout.create({
+							:order_id => order.number,
+							:status => "processing",
+							:transaction_id => response['tx']
+					}),
+        			:payment_method => payment_method })
+        		payment.started_processing!
+        	###########
+        	redirect_to "https://moolah.io/api/tx/#{response['tx']}"
+        	#redirect_to "#{response}"
+        	#For Production use
+        	#rescue => e
+        	#Rails.logger.error e
+        	# Redirect back to checkout so buyer can choose alternative payment method
+        	#redirect_to checkout_state_path(order.state)
         	end
+        end
 
     def success
     order = current_order
